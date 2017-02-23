d1d6da3c8c3de627e9952bd996e5946de94de388
matthieujan <matthieu.jan56@gmail.com>
2017-02-23 17:47:45 +0100

    Adding LevelGrid and Canvas

---
+++ src/data/LevelGrid.java
@@ -0,0 +1,24 @@
+package data;
+
+/**
+ * Project pacmanProjectInfo1, LevelGrid
+ * Created on Thu 2/23/17
+ *
+ * @author Matthieu Jan - matthieu.jan56@gmail.com
+ */
+public class LevelGrid {
+
+    private final char[][] map;
+
+    public LevelGrid(char[][] map){
+        this.map = map;
+    }
+
+    private boolean wallsArround(){
+        return false;
+    }
+
+    private boolean connexe(){
+        return false;
+    }
+}
