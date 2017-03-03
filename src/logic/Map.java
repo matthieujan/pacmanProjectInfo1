e50309e74c50f4d0a7869c119167cebbf2a019da
Feuillet Jules <jules.feuillet@gmail.com>
2017-03-03 10:23:06 +0100

    Map

--- src/logic/Map.java
+++ src/logic/Map.java
@@ -6,17 +6,6 @@ package logic;
 
    private char[][] map;
 
-   public Map(String filename) {
-     map = new char[10][10];
-     String line;
-     LineFileReader.open(filename);
-     for (int i=0; i<10; i++) {
-       line = LineFileReader.readLine();
-       for (int j=0; j<10; j++) {
-         map[i][j] = line.charAt(j);
-         System.out.print(map[i][j]); // pour verifier si le fichier est lu correctement
-       }
-       System.out.println("");
-     }
+   public Map() {
    }
  }
