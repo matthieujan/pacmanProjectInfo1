916aa394df9d9d08a6ea824037740d38d5506251
Deimos <matthieu.jan56@gmail.com>
2017-03-03 10:07:00 +0100

    Modif package logic : rules et characterState

--- src/logic/Map.java
+++ src/logic/Map.java
@@ -2,21 +2,9 @@ package logic;
 /**
  * A map containing items
  */
- public class Map {
+public class Map {
 
-   private char[][] map;
-
-   public Map(String filename) {
-     map = new char[10][10];
-     String line;
-     LineFileReader.open(filename);
-     for (int i=0; i<10; i++) {
-       line = LineFileReader.readLine();
-       for (int j=0; j<10; j++) {
-         map[i][j] = line.charAt(j);
-         System.out.print(map[i][j]); // pour verifier si le fichier est lu correctement
-       }
-       System.out.println("");
-     }
-   }
- }
+    public String get(int x, int y){
+        return null;
+    }
+}
