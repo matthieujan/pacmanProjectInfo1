af9a9fbfe15c597838e67a122ba9b94091f7dc78
Deimos <matthieu.jan56@gmail.com>
2017-03-11 18:19:59 +0100

    Creation Basic Data

--- src/view/GumView.java
+++ src/view/GumView.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.*;
 import java.awt.geom.*;
 
 public class GumView extends Figure {
@@ -8,10 +9,8 @@ public class GumView extends Figure {
 
   public GumView(int size, int x, int y) {
     super(size, size, x, y, "white");
-    this.gumShape = new Ellipse2D.Double(i*1.4*BasicView.getMapSize()/500, j*1.4*BasicView.getMapSize()/500, 0.2*BasicView.getMapSize()/500, 0.2*BasicView.getMapSize()/500);
   }
 
   public void draw() {
-    BasicView.getCanvas.draw(this, color, ghostShape);
   }
 }
